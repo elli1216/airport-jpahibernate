b6c71fbc96ecfce2d034e39ea164b33432717ec2
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2026-05-06 12:52:11 +0800

    extend one entity

--- src/main/java/com/darlellison/Main.java
+++ src/main/java/com/darlellison/Main.java
@@ -6,7 +6,6 @@ import com.darlellison.airport.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.hibernate.sql.results.graph.collection.internal.ArrayInitializerProducer;
 
 import java.time.LocalDate;
 
@@ -17,11 +16,7 @@ public class Main {
         ) {
             em.getTransaction().begin();
 
-            Airport NAIA = new Airport();
-            NAIA.setName("Ninoy Aquino International Airport");
-
             Passenger darl = new Passenger("Darl Floresca");
-            darl.setAirport(NAIA);
             Address address = new Address();
 
             address.setStreet("Switch");
@@ -29,18 +24,27 @@ public class Main {
             address.setZipCode("3019");
             address.setCity("Marilao");
 
-            TicketKey keyOfVIP = new TicketKey();
-            keyOfVIP.setNumber("1111");
-            keyOfVIP.setSeries("VIPS");
+            TicketKey keyOfOneWay = new TicketKey();
+            keyOfOneWay.setNumber("1234");
+            keyOfOneWay.setSeries("EERD");
 
-            VIPTicket vipTicket = new VIPTicket();
-            vipTicket.setVIPLatestDepartureDate(LocalDate.of(2026, 5, 12));
-            vipTicket.setId(keyOfVIP);
+            TicketKey keyOfReturn = new TicketKey();
+            keyOfReturn.setNumber("5678");
+            keyOfReturn.setSeries("POIU");
 
-            vipTicket.addPassenger(darl);
+            OneWayTicket oneWayTicket = new OneWayTicket();
+            oneWayTicket.setId(keyOfOneWay);
+            oneWayTicket.setLatestDepartureDate(LocalDate.of(2026, 5, 15));
 
-            em.persist(NAIA);
-            em.persist(vipTicket);
+            ReturnTicket returnTicket = new ReturnTicket();
+            returnTicket.setLatestReturnDate(LocalDate.of(2026, 6, 15));
+            returnTicket.setId(keyOfReturn);
+
+            darl.setOneWayTicket(oneWayTicket);
+            darl.setReturnTicket(returnTicket);
+
+            em.persist(oneWayTicket);
+            em.persist(returnTicket);
             em.persist(darl);
 
             em.getTransaction().commit();
