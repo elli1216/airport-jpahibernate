89ddd1d956e56cadaa63a74eead705bce0fc022d
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2026-05-06 19:08:39 +0800

    add app managed EntityManager (countries)

---
+++ src/main/java/com/darlellison/airport/Country.java
@@ -0,0 +1,64 @@
+package com.darlellison.airport;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String codeName;
+
+    public Country() {}
+
+    public Country(String name, String codeName) {
+        this.name = name;
+        this.codeName = codeName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+
+        return Objects.equals(name, country.name) &&
+                Objects.equals(codeName, country.codeName);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(name, codeName); }
+}
